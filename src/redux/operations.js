95edd9f0bd6b5598b7e524251d6f1b6d00a554f1
Macbook <108225103+Kate-Vabishchevich@users.noreply.github.com>
2023-03-29 23:11:29 +0300

    add operations

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -10,7 +10,31 @@ export const fetchContacts = createAsyncThunk(
       const response = await axios.get('/contacts');
       return response.data;
     } catch (error) {
-      thunkApi.rejectWithValue();
+      return thunkApi.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const addContacts = createAsyncThunk(
+  'contacts/addContact',
+  async (newContact, thunkApi) => {
+    try {
+      const response = await axios.get('/contacts', newContact);
+      return response.data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const deleteContacts = createAsyncThunk(
+  'contacts/deleteContact',
+  async (id, thunkApi) => {
+    try {
+      const response = await axios.get(`/contacts/${id}`);
+      return response.data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error.message);
     }
   }
 );
