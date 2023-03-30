19c3179eb434cbe2a9ebf85a10035d52b70246b1
Macbook <108225103+Kate-Vabishchevich@users.noreply.github.com>
2023-03-30 16:49:34 +0300

    fix selector

--- src/components/MyContacts/ContactsList/ContactsList.jsx
+++ src/components/MyContacts/ContactsList/ContactsList.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { deleteContact, getContacts } from 'redux/contactsSlice';
 import css from './ContactsList.module.css';
-import { getFilterValue } from 'redux/filterSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteContact, fetchContacts } from 'redux/operations';
+import {
+  getFilterValue,
+  getContacts,
+  getIsLoading,
+  getError,
+} from 'redux/selectors';
+import { useEffect } from 'react';
+import Loader from '../Loader/Loader';
 
 const ContactsList = () => {
-  const contacts = useSelector(getContacts);
   const dispatch = useDispatch();
+  const contacts = useSelector(getContacts);
   const filter = useSelector(getFilterValue);
+  const isLoading = useSelector(getIsLoading);
+  const onError = useSelector(getError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  // const normalisedFilter = filter.toLowerCase();
+  // const filteredContacts = contacts?.filter(item =>
+  //   item.name.toLowerCase().includes(normalisedFilter)
+  // );
 
   const filteredContacts = () => {
     const normalizedFilter = filter.toLowerCase();
@@ -17,16 +34,20 @@ const ContactsList = () => {
     if (filtered.length === 0 && filter) {
       alert('There are no matches');
     }
+    if (onError) {
+      alert('Something wrong');
+    }
     return filtered;
   };
 
   return (
     <>
+      {isLoading && <Loader />}
       <ol className={css.list}>
-        {filteredContacts().map(({ name, number, id }) => {
+        {filteredContacts().map(({ name, phone, id }) => {
           return (
             <li className={css.item} key={id}>
-              {name}: {number}
+              {name}: {phone}
               <button
                 className={css.btn}
                 onClick={() => dispatch(deleteContact(id))}
